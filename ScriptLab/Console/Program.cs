d4c84d6d5dd3909564e72fb2a603ee0adfa3ddf9
Pierre Arnaud <arnaud@epsitec.ch>
2017-02-10 06:15:47 +0100

    Add basic code structure

---
+++ ScriptLab/Console/Program.cs
@@ -0,0 +1,14 @@
+﻿//	Copyright © 2017, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+namespace ScriptLab
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var func = ScriptCompiler.Compile<decimal> ("42M");
+			System.Console.WriteLine (func ());
+		}
+	}
+}
---
+++ ScriptLab/Library/ScriptCompiler.cs
@@ -0,0 +1,13 @@
+﻿//	Copyright © 2017, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+namespace ScriptLab
+{
+	public static class ScriptCompiler
+	{
+		public static System.Func<T> Compile<T>(string source)
+		{
+			return () => default (T);
+		}
+	}
+}
